980f0e52a391ce28ad1aab7a1e36f80fa3e2c7f5
Marcos Junior <marcosjunior538@gmail.com>
2022-02-21 16:16:23 -0300

    SQL V2.0

--- Recruta.sql
+++ Recruta.sql
@@ -4,21 +4,28 @@ USE recruta;
 
 CREATE TABLE `usuario` (
     `id` int(10) AUTO_INCREMENT, 
+    `nome` varchar(100) NOT NULL, 
     `login` varchar(50) NOT NULL, 
     `senha` varchar(15) NOT NULL, 
     `candidato_recrutador` BOOLEAN DEFAULT false, 
     CONSTRAINT usuario_pk (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `recrutador` (
+CREATE TABLE `vagas` (
     `id` int(10) AUTO_INCREMENT,
-    `nome` varchar(50) NOT NULL, 
+    `id_recrutador` int(10) NOT NULL,
+    `nome_recrutador` varchar(50) NOT NULL, 
     `empresa` varchar(50) NOT NULL, 
-    CONSTRAINT recrutador_pk primary key (id),
-    CONSTRAINT `recutador_recrutadorid_fk` FOREIGN KEY (`recrutador_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE,
-)
+    `informacoes` varchar(255),
+    `localidade` varchar(100),
+    `salario` float(4,2),
+    `interesse` varchar(255) NOT NULL, 
+    CONSTRAINT vaga_pk primary key (id),
+    CONSTRAINT `vaga_recrutadorid_fk` FOREIGN KEY (`recrutador_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `vaga_nomerecrutador_fk` FOREIGN KEY (`nome_recrutador`) REFERENCES `usuario` (`nome`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `candidato` (
+CREATE TABLE `perfil_candidato` (
     `id` int(10) AUTO_INCREMENT,
     `nome` varchar(100) DEFAULT NULL, 
     `descricao` text(255),
@@ -28,20 +35,7 @@ CREATE TABLE `candidato` (
     `interesse` varchar(255),
     CONSTRAINT candidado_pk primary key (id)
     CONSTRAINT `candidato_candidatoid_fk` FOREIGN KEY (`candidato_id`) REFERENCES `candidato` (`id`) ON DELETE CASCADE,
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `vagas` (
-    `id` int(10) AUTO_INCREMENT,
-    `recrutador_id` int(10),
-    `empresa` varchar(100),
-    `descricao` text(255),
-    `email` varchar(50),
-    `localidade` varchar(50),
-    `salario` int(10),
-    `interesse` varchar(255),
-    CONSTRAINT vagas_pk primary key (id),
-    CONSTRAINT `recutador_recrutadorid_fk` FOREIGN KEY (`recrutador_id`) REFERENCES `recrutador` (`id`) ON DELETE CASCADE,
-    CONSTRAINT `empresa_empresa_fk` FOREIGN KEY (`empresa_nome`) REFERENCES `recrutador` (`empresa`) ON DELETE CASCADE
+    CONSTRAINT `vaga_nomerecandidato_fk` FOREIGN KEY (`nome_candidato`) REFERENCES `usuario` (`nome`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `compatibilidade` (
@@ -53,7 +47,7 @@ CREATE TABLE `compatibilidade` (
     `interesse_recrutador` varchar(255),
     `percentual_compatibilidade` float (4,2),
     CONSTRAINT compatibilidade_pk primary key (id),
-    CONSTRAINT `descricaovaga_fk` FOREIGN KEY (`descricao_vaga`) REFERENCES `vaga` (`descricao`) ON DELETE CASCADE,
+    CONSTRAINT `descricaovaga_fk` FOREIGN KEY (`descricao_vaga`) REFERENCES `vagas` (`descricao`) ON DELETE CASCADE,
     CONSTRAINT `interessecandidato_fk` FOREIGN KEY (`interesse_candidato`) REFERENCES `candidato` (`interesse`) ON DELETE CASCADE,
-    CONSTRAINT `interesserecrutador_fk` FOREIGN KEY (`interesse_recrutador`) REFERENCES `recrutador` (`interesse`) ON DELETE CASCADE,
+    CONSTRAINT `interesserecrutador_fk` FOREIGN KEY (`interesse_recrutador`) REFERENCES `vagas` (`interesse`) ON DELETE CASCADE,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
