5ce02d0ae2b92dad9b7c222857d0433da7756509
Marcos Junior <marcosjunior538@gmail.com>
2022-03-07 21:40:58 -0300

    Reestruturação do banco de dados e Ajuste das funcionalidades de usuário

--- Recruta.sql
+++ Recruta.sql
@@ -1,53 +1,98 @@
 CREATE DATABASE recruta; 
 
-USE recruta;
+USE recruta; 
 
-CREATE TABLE `usuario` (
-    `id` int(10) AUTO_INCREMENT, 
-    `nome` varchar(100) NOT NULL, 
-    `login` varchar(50) NOT NULL, 
-    `senha` varchar(15) NOT NULL, 
-    `candidato_recrutador` boolean DEFAULT false, 
-    CONSTRAINT usuario_pk primary key (id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `login` VARCHAR(100) NOT NULL,
+  `senha` VARCHAR(100) NOT NULL,
+  `nome` VARCHAR(100) NOT NULL,
+  `recrutador` BOOLEAN NOT NULL DEFAULT 0,
+  `cidade` VARCHAR(100) NULL,
+  `estado` VARCHAR(2) NULL,
+  `verificado` BOOLEAN NOT NULL DEFAULT 0,
+  `telefone` VARCHAR(15),
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  UNIQUE INDEX `login_UNIQUE` (`login` ASC)) ENGINE = InnoDB;
 
-CREATE TABLE `vagas` (
-    `id` int(10) AUTO_INCREMENT,
-    `id_recrutador` int(10) NOT NULL,
-    `nome_recrutador` varchar(50) NOT NULL, 
-    `empresa` varchar(50) NOT NULL, 
-    `informacoes` varchar(255),
-    `localidade` varchar(100),
-    `salario` float(4,2),
-    `interesse` varchar(255) NOT NULL, 
-    CONSTRAINT vaga_pk primary key (id),
-    CONSTRAINT `vaga_recrutadorid_fk` FOREIGN KEY (`recrutador_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE,
-    CONSTRAINT `vaga_nomerecrutador_fk` FOREIGN KEY (`nome_recrutador`) REFERENCES `usuario` (`nome`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `candidato` (
+  `Usuario_id` INT NOT NULL,
+  `grau_formacao` VARCHAR(100) NOT NULL,
+  `instituicao_ensino` VARCHAR(100) NOT NULL,
+  `tag1` VARCHAR(100) NULL,
+  `tag2` VARCHAR(100) NULL,
+  `tag3` VARCHAR(100) NULL,
+  `idade` INT NOT NULL,
+  `portifolio` VARCHAR(100) NULL,
+  PRIMARY KEY (`Usuario_id`),
+  CONSTRAINT `fk_Candidato_Usuario`
+    FOREIGN KEY (`Usuario_id`)
+    REFERENCES `Usuario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION) ENGINE = InnoDB;
 
-CREATE TABLE `perfil_candidato` (
-    `id` int(10) AUTO_INCREMENT,
-    `nome` varchar(100) DEFAULT NULL, 
-    `descricao` text(255),
-    `formacao` varchar(255) NOT NULL, 
-    `email` varchar(50),
-    `localidade` varchar(50),
-    `interesse` varchar(255),
-    CONSTRAINT candidado_pk primary key (id)
-    CONSTRAINT `candidato_candidatoid_fk` FOREIGN KEY (`candidato_id`) REFERENCES `candidato` (`id`) ON DELETE CASCADE,
-    CONSTRAINT `vaga_nomerecandidato_fk` FOREIGN KEY (`nome_candidato`) REFERENCES `usuario` (`nome`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `recrutador` (
+  `Usuario_id` INT NOT NULL,
+  `empresa` VARCHAR(100) NOT NULL,
+  `cargo` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`Usuario_id`),
+  CONSTRAINT `fk_Recrutador_Usuario1`
+    FOREIGN KEY (`Usuario_id`)
+    REFERENCES `Usuario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION) ENGINE = InnoDB;
 
-CREATE TABLE `compatibilidade` (
-    `id` int(10) AUTO_INCREMENT,
-    `recrutador_id` int(10),
-    `candidato_id`  int(10),
-    `descricao_vaga` text(255),
-    `interesse_candidato` varchar(255),
-    `interesse_recrutador` varchar(255),
-    `percentual_compatibilidade` float (4,2),
-    CONSTRAINT compatibilidade_pk primary key (id),
-    CONSTRAINT `descricaovaga_fk` FOREIGN KEY (`descricao_vaga`) REFERENCES `vagas` (`descricao`) ON DELETE CASCADE,
-    CONSTRAINT `interessecandidato_fk` FOREIGN KEY (`interesse_candidato`) REFERENCES `candidato` (`interesse`) ON DELETE CASCADE,
-    CONSTRAINT `interesserecrutador_fk` FOREIGN KEY (`interesse_recrutador`) REFERENCES `vagas` (`interesse`) ON DELETE CASCADE,
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `experiencia` (
+  `Candidato_Usuario_id` INT NOT NULL,
+  `cargo` VARCHAR(100) NULL,
+  `empresa` VARCHAR(100) NULL,
+  INDEX `fk_Experiencia_Candidato1_idx` (`Candidato_Usuario_id` ASC)  ,
+  CONSTRAINT `fk_Experiencia_Candidato1`
+    FOREIGN KEY (`Candidato_Usuario_id`)
+    REFERENCES `Candidato` (`Usuario_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `vaga` (
+  `id` INT NOT NULL,
+  `cargo` VARCHAR(100) NOT NULL,
+  `descricao` VARCHAR(255) NOT NULL,
+  `salario` FLOAT NULL,
+  `tipo` VARCHAR(100) NOT NULL,
+  `tag1` VARCHAR(45) NULL,
+  `tag2` VARCHAR(100) NULL,
+  `tag3` VARCHAR(100) NULL,
+  `cidade` VARCHAR(100) NULL,
+  `estado` VARCHAR(2) NULL,
+  `ativo` BOOLEAN DEFAULT 1,
+  `quantidade` INT NOT NULL,
+  `Recrutador_Usuario_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC)  ,
+  INDEX `fk_Vaga_Recrutador1_idx` (`Recrutador_Usuario_id` ASC)  ,
+  CONSTRAINT `fk_Vaga_Recrutador1`
+    FOREIGN KEY (`Recrutador_Usuario_id`)
+    REFERENCES `Recrutador` (`Usuario_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `processo` (
+  `Candidato_Usuario_id` INT NOT NULL,
+  `Vaga_id` INT NOT NULL,
+  `contato` BOOLEAN NOT NULL DEFAULT 0,
+  `devolutiva` BOOLEAN NOT NULL DEFAULT 0,
+  PRIMARY KEY (`Candidato_Usuario_id`, `Vaga_id`),
+  INDEX `fk_Candidato_has_Vaga_Vaga1_idx` (`Vaga_id` ASC)  ,
+  INDEX `fk_Candidato_has_Vaga_Candidato1_idx` (`Candidato_Usuario_id` ASC)  ,
+  CONSTRAINT `fk_Candidato_has_Vaga_Candidato1`
+    FOREIGN KEY (`Candidato_Usuario_id`)
+    REFERENCES `Candidato` (`Usuario_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Candidato_has_Vaga_Vaga1`
+    FOREIGN KEY (`Vaga_id`)
+    REFERENCES `Vaga` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION) ENGINE = InnoDB;
--- rotas/rotas.js
+++ rotas/rotas.js
@@ -1,71 +1,281 @@
-const {body, validationResult} = require("express-validator");
+const { body, validationResult } = require("express-validator");
 const banco = require("../bancoDeDados/conexao");
 
-module.exports = app => { 
+module.exports = app => {
 
-  //Página Inicial
-  app.post("/login", app.configuracao.autenticacao.login)
-  app.post(".validateToken", app.configuracao.autenticacao.validaChave)
+//Página Inicial
+    app.post("/login", app.configuracao.autenticacao.login)
+    app.post(".validateToken", app.configuracao.autenticacao.validaChave)
 
 
-  //<><><><><><><>SEÇÃO USUÁRIO<><><><><><><><><><>
+//<><><><><><><>SEÇÃO USUÁRIO<><><><><><><><><><>
 
-  //Criando Usuário - Recrutador 1 e Candidato 0 
-  app.route("/adicionarUsuario").post([
-      body("nome", "*Obrigatório").trim().isLength({min:2, max:100}),
-      body("login", "*Obrigatório").trim().isLength({min: 2, max:20}),
-      body("senha", "Senha deve ter entre 6 e 8 caracteres").trim().isLength({min:6,max:8}),
-      body("candidato_recrutador").trim(),
-  ],
-  async (req, res) => {
-      const erro = validationResult(req);
-      if (!erro.isEmpty()) {
-          res.status(400).send(erro.array())
-      } else { 
-          const resultado = await banco.inserirUsuario({
-              nome: req.body.nome, 
-              login: req.body.login, 
-              senha: req.body.senha,
-              candidato_recrutador: req.body.candidato_recrutador
-          });
-          if (resultado == "Login previamente cadastrado!"){
-              res.status(400).send(resultado)
-          }
-          res.status(200).send("Usuário inserido com sucesso!")
-      }
-  });
-
-  //Excluindo Usuário
-  app.route("/excluirUsuario/:id?")
-    .all(app.configuracao.passport.authenticate())
-    .delete(async (req, res) => {
-        if(req.params.id) {
-            const resultado = await banco.excluirUsuario(req.params.id);
-            if(resultado=="Id Inexistente!"){
-                res.status(400).send("Id Inexistente!")
-            }else{
-                res.send(resultado);
+    //Criando Usuário - Recrutador 1 e Candidato 0 
+    app.route("/adicionarUsuario").post([
+        body("login", "*Obrigatório ser o e-mail").trim().isLength({ min: 6, max: 100 }),
+        body("senha", "Senha deve ter entre 6 e 8 caracteres").trim().isLength({ min: 6, max: 8 }),
+        body("nome", "*Obrigatório").trim().isLength({ min: 2, max: 100 }),
+        body("recrutador").trim(),
+        body("cidade").trim().isLength({ min: 2, max: 100 }),
+        body("estado").trim().isLength({ min: 2, max: 2 }),
+        body("verificado").trim(),
+        body("telefone").trim().isLength({ min: 8, max: 15 }),
+    ],
+        async (req, res) => {
+            const erro = validationResult(req);
+            if (!erro.isEmpty()) {
+                res.status(400).send(erro.array())
+            } else {
+                const resultado = await banco.inserirUsuario({
+                    login: req.body.login,
+                    senha: req.body.senha,
+                    nome: req.body.nome,
+                    recrutador: req.body.recrutador,
+                    cidade: req.body.cidade, 
+                    estado: req.body.estado, 
+                    verificado: req.body.verificado,
+                    telefone: req.body.telefone
+                });
+                if (resultado == "Login previamente cadastrado!") {
+                    res.status(400).send(resultado)
+                }
+                
+                res.status(200).send("Usuário inserido com sucesso!")
+                
             }
-        }
-    });
+        });
+
+    //Excluindo Usuário
+    app.route("/excluirUsuario/:id?")
+        .all(app.configuracao.passport.authenticate())
+        .delete(async (req, res) => {
+            if (req.params.id) {
+                const resultado = await banco.excluirUsuario(req.params.id);
+                if (resultado == "Id Inexistente!") {
+                    res.status(400).send("Id Inexistente!")
+                } else {
+                    res.send(resultado);
+                }
+            }
+        });
 
     //Listar Todos os Usuários
     app.route("/listarUsuarios")
         .all(app.configuracao.passport.authenticate())
         .get(async (req, res) => {
-        const resultado = await banco.listarUsuarios();
-        res.send(resultado);
-    });
+            const resultado = await banco.listarUsuarios();
+            res.send(resultado);
+        });
 
     //Listar Um Usuário
     app.route("/listarUmUsuario/:id?")
         .all(app.configuracao.passport.authenticate())
         .get(async (req, res) => {
-            if(req.params.id) {
+            if (req.params.id) {
                 const resultado = await banco.listarUmUsuario(req.params.id);
                 res.send(resultado);
             } else {
                 res.send("Id Inexistente!")
             }
         });
-}
+};
+
+
+/*
+//<><><><><><><>SEÇÃO VAGAS<><><><><><><><><><>
+//<><><><><><><>SOMENTE RECRUTADOR (0) TEM ESSA VISÃO<><><><><><>
+
+    //Inserir Vagas
+    app.route("/adicionarVaga")
+        .all(app.configuracao.passport.authenticate())
+        .post([
+            body("id_recrutador").trim().isLength({ min: 1 }),
+            body("nome_recrutador").trim().isLength({ min: 1 }),
+            body("empresa", "*Campo obrigatório").trim().isLength({ min: 2, max: 100 }),
+            body("informacoes").trim().isLength({ min: 2, max: 255 }),
+            body("localidade").trim().isLength({ min: 2, max: 100 }),
+            body("salario").isLength({ min: 2 }),
+            body("interesse").trim().isLength({ min: 1, max: 255 }),],
+            async (req, res) => {
+                const erro = validationResult(req);
+
+                if (!erro.isEmpty()) {
+                    res.send(erro.array())
+                } else {
+                    const resultado = await banco.inserirVaga({
+                        id_recrutador: req.body.id_recrutador,
+                        nome_recrutador: req.body.nome_recrutador,
+                        empresa: req.body.empresa,
+                        informacoes: req.body.informacoes,
+                        localidade: req.body.localidade,
+                        salario: req.body.salario,
+                        interesse: req.body.interesse
+                    });
+                    res.send(resultado);
+                }
+            });
+
+    //Excluir Vagas
+    app.route("/excluirVaga/:id?")
+        .all(app.configuracao.passport.authenticate())
+        .delete(async (req, res) => {
+            if (req.params.id) {
+                const resultado = await banco.excluirVaga(req.params.id);
+                if (resultado == "Id inexistente!") {
+                    res.status(400).send("Id inexistente!")
+                } else {
+                    res.send(resultado);
+                }
+            }
+        });
+
+    //Alterar Vagas
+    app.route("/alterarVaga")
+        .all(app.configuracao.passport.authenticate())
+        .put([
+            body("id").trim(),
+            body("id_recrutador").trim().isLength({ min: 1 }),
+            body("nome_recrutador").trim().isLength({ min: 1 }),
+            body("empresa", "*Campo obrigatório").trim().isLength({ min: 2, max: 100 }),
+            body("informacoes").trim().isLength({ min: 2, max: 255 }),
+            body("localidade").trim().isLength({ min: 2, max: 100 }),
+            body("salario").isLength({ min: 2 }),
+            body("interesse").trim().isLength({ min: 1, max: 255 }),],
+
+            async (req, res) => {
+                const erro = validationResult(req);
+
+                if (!erro.isEmpty()) {
+                    res.send(erro.array())
+                } else {
+                    const resultado = await banco.alterarVaga({
+                        id: req.body.id,
+                        id_recrutador: req.body.id_recrutador,
+                        nome_recrutador: req.body.nome_recrutador,
+                        empresa: req.body.empresa,
+                        informacoes: req.body.informacoes,
+                        localidade: req.body.localidade,
+                        salario: req.body.salario,
+                        interesse: req.body.interesse
+                    });
+                    res.send(resultado);
+                }
+            });
+
+    //Listar Todas as Vagas
+    app.route("/listarVaga")
+        .all(app.configuracao.passport.authenticate())
+        .get(async (req, res) => {
+            const resultado = await banco.listarVaga();
+            res.send(resultado);
+        });
+
+    //Listar Uma Vaga
+    app.route("/listarUmaVaga/:id?")
+        .all(app.configuracao.passport.authenticate())
+        .get(async (req, res) => {
+
+            if (req.params.id) {
+                const resultado = await banco.listarUmaVaga(req.params.id);
+                res.send(resultado);
+            } else {
+                res.send("Id Inexistente!")
+            }
+        });
+
+//<><><><><><><>SEÇÃO PERFIL DO CANDIDATO<><><><><><><><><><>
+//<><><><><><><>SOMENTE CANDIDATO (1) TEM ESSA VISÃO<><><><><><>
+
+        //Inserir Perfil
+        app.route("/adicionarPerfil")
+        .all(app.configuracao.passport.authenticate())
+        .post([
+            body("nome_candidato").trim().isLength({ min: 2 , max: 100 }),
+            body("descrição").trim().isLength({ min: 2, max: 255 }),
+            body("formação").trim().isLength({ min: 2, max: 255 }),
+            body("email").trim().isLength({ min: 8, max: 50 }),
+            body("localidade").trim().isLength({ min: 2, max: 100 }),
+            body("interesse").trim().isLength({ min: 1, max: 255 }),],
+            async (req, res) => {
+                const erro = validationResult(req);
+
+                if (!erro.isEmpty()) {
+                    res.send(erro.array())
+                } else {
+                    const resultado = await banco.inserirVaga({
+                        nome_candidato: req.body.nome_candidato,
+                        descricao: req.body.descricao,
+                        formacao: req.body.formacao,
+                        email: req.body.email,
+                        localidade: req.body.localidade,
+                        interesse: req.body.interesse
+                    });
+                    res.send(resultado);
+                }
+            });
+
+    //Excluir Perfil de Candidato
+    app.route("/excluirPerfil/:id?")
+        .all(app.configuracao.passport.authenticate())
+        .delete(async (req, res) => {
+            if (req.params.id) {
+                const resultado = await banco.excluirPerfil(req.params.id);
+                if (resultado == "Id inexistente!") {
+                    res.status(400).send("Id inexistente!")
+                } else {
+                    res.send(resultado);
+                }
+            }
+        });
+
+    //Alterar Perfil de Candidato
+    app.route("/alterarPerfil")
+        .all(app.configuracao.passport.authenticate())
+        .put([
+            body("id").trim(),
+            body("nome_candidato").trim().isLength({ min: 2 , max: 100 }),
+            body("descrição").trim().isLength({ min: 2, max: 255 }),
+            body("formação").trim().isLength({ min: 2, max: 255 }),
+            body("email").trim().isLength({ min: 8, max: 50 }),
+            body("localidade").trim().isLength({ min: 2, max: 100 }),
+            body("interesse").trim().isLength({ min: 1, max: 255 }),],
+
+            async (req, res) => {
+                const erro = validationResult(req);
+
+                if (!erro.isEmpty()) {
+                    res.send(erro.array())
+                } else {
+                    const resultado = await banco.alterarVaga({
+                        id: req.body.id,
+                        nome_candidato: req.body.nome_candidato,
+                        descricao: req.body.descricao,
+                        formacao: req.body.formacao,
+                        email: req.body.email,
+                        localidade: req.body.localidade,
+                        interesse: req.body.interesse
+                    });
+                    res.send(resultado);
+                }
+            });
+
+    //Listar Todos os Candidatos
+    app.route("/listarPerfil")
+        .all(app.configuracao.passport.authenticate())
+        .get(async (req, res) => {
+            const resultado = await banco.listarPerfil();
+            res.send(resultado);
+        });
+
+    //Listar Um Perfil
+    app.route("/listarUmPerfil/:id?")
+        .all(app.configuracao.passport.authenticate())
+        .get(async (req, res) => {
+
+            if (req.params.id) {
+                const resultado = await banco.listarUmPerfil(req.params.id);
+                res.send(resultado);
+            } else {
+                res.send("Id Inexistente!")
+            }
+        };*/
