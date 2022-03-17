4082248abc98c0de46d2c859ee90495ae1046b2e
williansumida <sumidawillian@gmail.com>
2022-03-17 12:35:08 -0300

    user conexao banco

--- bancoDeDados/conexao.js
+++ bancoDeDados/conexao.js
@@ -4,7 +4,7 @@ async function conecta() {
     const con = await banco.createConnection({
         host: "localhost",
         port: 3306,
-        user: "sa",
+        user: "root",
         password: "root",
         database: "recruta"
     })
